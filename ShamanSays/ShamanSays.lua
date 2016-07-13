e6ad4406e5a48606fa9362508834edbe5930a6b1
Semiceri <semisnowy@gmail.com>
2016-07-13 13:55:36 -0700

    inital commit, hello world

---
+++ ShamanSays/ShamanSays.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: semiceri
+-- Date: 7/13/2016
+-- Time: 1:36 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+message('HelloWorld')
