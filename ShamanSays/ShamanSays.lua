aefe292ca04aa11d632b11fc1ea461029fb1f357
Semiceri <semisnowy@gmail.com>
2016-07-14 01:26:29 -0700

    adding in cap totem countdown logic

--- ShamanSays/ShamanSays.lua
+++ ShamanSays/ShamanSays.lua
@@ -5,17 +5,49 @@
 -- Time: 1:36 PM
 --
 
---function that should be swapped out
+--leaving these here could cause syncing issues
+local capTotemPlaced, capTotemTimer, capTotemCoundown = false, 0, 5
+
+
+--TODO: update this function for say chat
 local print = function (msg)
     DEFAULT_CHAT_FRAME:AddMessage(msg)
+end
+
+local function spellcastSuccededEvent(self, event, ...)
+    local unitID, spell, rank, lineID, spellID = ...;
+    --Find abilities here
+    if (spell == "Capacitor Totem") then
+        capTotemPlaced = true
+    end
+end
+
+
+--I feel like this is a bad idea but lets do it anyway
+local function onUpdate(self, elapsed)
+    if capTotemPlaced then
+        capTotemTimer = capTotemTimer + elapsed
+        if capTotemTimer >= 1 then
+            print(capTotemCoundown)
+            capTotemCoundown = capTotemCoundown - 1
+            capTotemTimer = 0
+        end
+        if capTotemCoundown <= 0 then
+            capTotemPlaced = false
+        end
+    end
 end
 
 local frame = CreateFrame("FRAME", "ShammyFrame");
 frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
-local function spellcastSuccededEvent(self, event, ...)
-    local unitID, spell, rank, lineID, spellID = ...;
-    print(spell)
+
+--passthough function for all events
+local function eventTriggered(self, event, ...)
+    if event == "UNIT_SPELLCAST_SUCCEEDED" then
+        spellcastSuccededEvent(self, event, ...)
+    end
 end
 
-frame:SetScript("OnEvent", spellcastSuccededEvent)
+frame:SetScript("OnEvent", eventTriggered)
+
