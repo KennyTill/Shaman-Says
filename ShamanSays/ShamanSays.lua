35c336df65f32f54e472e4cdf4e041685b09e5ee
Semiceri <semisnowy@gmail.com>
2016-07-13 21:18:04 -0700

    fianlly manage to pull out the spell names

--- ShamanSays/ShamanSays.lua
+++ ShamanSays/ShamanSays.lua
@@ -10,31 +10,12 @@ local print = function (msg)
     DEFAULT_CHAT_FRAME:AddMessage(msg)
 end
 
-is_casting = false
-SpellTrackerFunctions = { }
+local frame = CreateFrame("FRAME", "ShammyFrame");
+frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
-SpellTracker = CreateFrame("Frame", nil, UIParent)
-SpellTracker:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-SpellTracker:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-SpellTracker:RegisterEvent("UNIT_SPELLCAST_FAILED")
-SpellTracker:RegisterEvent("UNIT_SPELLCAST_START")
-SpellTracker:SetScript("OnEvent", function (_,e) SpellTrackerFunctions[e]() end)
-
-----
-function SpellTrackerFunctions.UNIT_SPELLCAST_SUCCEEDED(...)
-    local unitID, spell, rank, lineID, spellID = select(1, ...)
-    if not is_casting then
-        print("instant cast spell")
-    end
-    is_casting = false
-end
-----
-function SpellTrackerFunctions.UNIT_SPELLCAST_START()
-    is_casting = true
-end
-----
-function SpellTrackerFunctions.UNIT_SPELLCAST_FAILED()
-    is_casting = false
+local function spellcastSuccededEvent(self, event, ...)
+    local unitID, spell, rank, lineID, spellID = ...;
+    print(spell)
 end
 
-
+frame:SetScript("OnEvent", spellcastSuccededEvent)
