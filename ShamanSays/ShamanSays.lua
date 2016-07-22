a4ea7b2a38c84f0cf461388ea2cefb6f7a9ad501
Semiceri <semisnowy@gmail.com>
2016-07-22 16:35:00 -0700

    chat commands are registering but attempting to add AceEvent-3.0 mixin breaks all functionality

--- ShamanSays/ShamanSays.lua
+++ ShamanSays/ShamanSays.lua
@@ -6,16 +6,19 @@
 --
 
 
-ShamanSays = LibStub("AceAddon-3.0"):NewAddon("ShamanSays", "AceConsole-3.0", "AceEvent-3.0")
+ShamanSays = LibStub("AceAddon-3.0"):NewAddon("ShamanSays","AceConsole-3.0") -- ,"AceEvent-3.0")
 
-function ShamanSays:UNIT_SPELLCAST_SUCCEEDED(...)
-    self:Print("spellcast event caught!")
-    local unitID, spell, rank, lineID, spellID = ...;
-    --Find abilities here
-    if (spell == "Healing Surge") then
-       self:Print(spell)
-    end
-end
+
+--function ShamanSays:UNIT_SPELLCAST_SUCCEEDED()
+    --DEFAULT_CHAT_FRAME:AddMessage("caught event!")
+--    --self:Print(input)
+--    self:Print("spellcast event caught!")
+--    local unitID, spell, rank, lineID, spellID = input;
+--    --Find abilities here
+--    if (spell == "Healing Surge") then
+--       self:Print(spell)
+--    end
+--end
 
 function ShamanSays:HandleChatCommandFunc(input)
     --if branch to update stuff here
@@ -25,7 +28,8 @@ end
 
 function ShamanSays:OnInitialize()
     --registers for slash commands
-    ShamanSays:RegisterChatCommand("shamansays", "HandleChatCommandFunc",true)
+    self:RegisterChatCommand("shs", "HandleChatCommandFunc",true)
     --registers for events
-    ShamanSays:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+    DEFAULT_CHAT_FRAME:AddMessage("init!")
+    --self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 end
